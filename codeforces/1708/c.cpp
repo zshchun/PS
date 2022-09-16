0b1aa6e973583ad922eb80d06433cc1062c01ed1
Seunghwan Chun <zshchun@gmail.com>
2022-09-16 22:01:45 +0900

    Codeforces Round #808 (Div. 2) 1708 C #2
    https://codeforces.com/contest/1708

--- codeforces/1708/c.cpp
+++ codeforces/1708/c.cpp
@@ -7,36 +7,57 @@ typedef pair<ll,ll> pa;
 #define endl "\n"
 #define MOD 1000000007
 #define INF (1LL<<60)
-ll n, m, ans;
+ll n, m, q;
+
+bool check(vector<ll> &a, vector<ll> &ans, ll m) {
+	ll i, nq = q;
+	for (i=0;i<n;i++)
+		ans[i] = 0;
+	for (i=0;i<m;i++) {
+		if (a[i] <= nq) 
+			ans[i] = 1;
+		else
+			ans[i] = 0;
+	}
+	for (i=m;i<n;i++) {
+		if (a[i] <= nq) {
+			ans[i] = 1;
+		} else {
+			ans[i] = 1;
+			nq--;
+		}
+	}
+	if (nq >= 0)
+		return true;
+	else
+		return false;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	ll TC, i, j, k, q, l;
+	ll TC, i, j, k, l, r;
 	cin >> TC;
 	while(TC--) {
 		cin >> n >> q;
-		ans = 0;
 		vector<ll> a(n);
-		vector<ll> b(n+1);
-		string s;
-		k = 0;
-		for (i=0;i<n;i++) {
+		vector<ll> ans(n);
+		for (i=0;i<n;i++)
 			cin >> a[i];
-			if (a[i] <= q)
-				s += "1";
-			else
-				s += "0";
-		}
-
-		ll k = 0;
-		for (i=n-1;i>=0;i--) {
-			if (a[i] > k && k < q) {
-				s[i] = '1';
-				k++;
+		l = 0;
+		r = n-1;
+		while (l<=r) {
+			m = (l + r) / 2;
+			if (check(a, ans, m)) {
+				r = m - 1;
+			} else {
+				l = m + 1;
 			}
 		}
-		cout << s << endl;
+		check(a, ans, l);
+		for (i=0;i<n;i++)
+			cout << ans[i];
+		cout << endl;
 	}
 	return 0;
 }
