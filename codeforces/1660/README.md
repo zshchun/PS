ee27924ddd47e6275f5f62c9d1d87ce309f5f491
Seunghwan Chun <zshchun@gmail.com>
2022-10-02 21:54:47 +0900

    Codeforces Round #780 (Div. 3) 1660 A
    https://codeforces.com/contest/1660

---
+++ codeforces/1660/README.md
@@ -0,0 +1,2 @@
+# Codeforces Round #780 (Div. 3) 1660
+[Dashboard](https://codeforces.com/contest/1660)
---
+++ codeforces/1660/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __uint128_t u128;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define MOD 1000000007
+#define INF (1LL<<60)
+ll n, m, ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll TC, i, j, k, a, b;
+	cin >> TC;
+	while(TC--) {
+		cin >> a >> b;
+		if (a == 0) {
+			cout << 1 << endl;
+		} else {
+			cout << a+1 + b*2 << endl;
+		}
+	}
+	return 0;
+}
