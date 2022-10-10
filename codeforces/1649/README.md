9674b9ff337f2294bd10db3a10c213de5fe9fc96
Seunghwan Chun <zshchun@gmail.com>
2022-10-10 22:32:11 +0900

    Codeforces Round #775 (Div. 2, based on Moscow Open Olympiad in Informatics) 1649 A
    https://codeforces.com/contest/1649

---
+++ codeforces/1649/README.md
@@ -0,0 +1,2 @@
+# Codeforces Round #775 (Div. 2, based on Moscow Open Olympiad in Informatics) 1649
+[Dashboard](https://codeforces.com/contest/1649)
---
+++ codeforces/1649/a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __uint128_t u128;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define MOD 1000000007
+#define INF (1LL<<60)
+ll n, m, ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll TC, i, j, k;
+	cin >> TC;
+	while(TC--) {
+		cin >> n;
+		vector<ll> a(n+1);
+		for (i=1;i<=n;i++)
+			cin >> a[i];
+		for (i=1;i<=n;i++)
+			if (a[i] == 0)
+				break;
+		for (j=n;j>=1;j--)
+			if (a[j] == 0)
+				break;
+		ans = max(0ll, (j+1) - (i-1));
+		cout << ans << endl;
+	}
+	return 0;
+}
