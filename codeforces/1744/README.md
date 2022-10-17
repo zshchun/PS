1a18890ebcf6d2c88908cb88a4fb8782c60bb29c
Seunghwan Chun <zshchun@gmail.com>
2022-10-17 12:56:00 +0900

    Codeforces Round  #828 (Div. 3) 1744 A
    https://codeforces.com/contest/1744

---
+++ codeforces/1744/README.md
@@ -0,0 +1,2 @@
+# Codeforces Round  #828 (Div. 3) 1744
+[Dashboard](https://codeforces.com/contest/1744)
---
+++ codeforces/1744/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __uint128_t u128;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define MOD 1000000007
+#define INF (1LL<<60)
+ll n, m, ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll TC, i, j, k;
+	cin >> TC;
+	while(TC--) {
+		cin >> n;
+		ans = 0;
+		vector<ll> a(51);
+		vector<ll> b(51);
+		string s;
+		for (i=0;i<n;i++) {
+			cin >> a[i];
+		}
+		cin >> s;
+		bool ok = true;
+		for (i=0;i<n;i++) {
+			j = a[i];
+			if (!b[j]) {
+				b[j] = s[i];
+			} else if (b[j] != s[i]) {
+				ok = false;
+			}
+		}
+		if (ok)
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+	return 0;
+}
