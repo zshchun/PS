628f325b31afb110480382fa6d8715b8e9f34cef
Seunghwan Chun <zshchun@gmail.com>
2022-10-26 23:44:54 +0900

    Codeforces Round #829 (Div. 2) 1754 B
    https://codeforces.com/contest/1754

---
+++ codeforces/1754/b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __uint128_t u128;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define MOD 1000000007
+#define INF (1LL<<60)
+ll n, m, ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll TC, i, j, k;
+	cin >> TC;
+	while(TC--) {
+		cin >> n;
+		for (i=0;i<n;i++) {
+			if (i%2)
+				cout << i/2+1 << ' ';
+			else
+				cout << n/2+i/2+1 << ' ';
+		}
+		cout << endl;
+	}
+	return 0;
+}
