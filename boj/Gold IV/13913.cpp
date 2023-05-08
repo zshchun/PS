21f998d06148bf174581ffd43f78ae5e445e42f1
Seunghwan Chun <zshchun@gmail.com>
2023-05-09 01:22:44 +0900

    BOJ 13913 숨바꼭질 4 (Gold IV) #2
    https://www.acmicpc.net/problem/13913

--- boj/Gold IV/13913.cpp
+++ boj/Gold IV/13913.cpp
@@ -2,67 +2,48 @@
 using namespace std;
 typedef long long ll;
 typedef pair<ll,ll> pa;
-#define INF 1234567890
 #define endl "\n"
-ll N, K;
-
-int m[100001];
-int o[100001];
+int N, K;
+int visited[100001];
+int p[100001];
 
 void bfs() {
-	int cur;
+	int cur, i;
 	queue<int> q;
 	q.push(N);
-	m[N] = -1;
-	o[N] = 0;
+	visited[N] = 0;
 	while (!q.empty()) {
 		cur = q.front();
 		q.pop();
-		if (cur*2 <= 100000 && (o[cur] + 1) < o[cur*2]) { // && cur < K) {
-			q.push(cur*2);
-			m[cur*2] = cur;
-			o[cur*2] = o[cur] + 1;
-		}
-		if (cur+1 <= 100000 && (o[cur] + 1) < o[cur+1]) { // && cur < K) {
-			q.push(cur+1);
-			m[cur+1] = cur;
-			o[cur+1] = o[cur] + 1;
-		}
-		if (cur-1 >= 0 && (o[cur] + 1) < o[cur-1]) { // && cur > K) {
-			q.push(cur-1);
-			m[cur-1] = cur;
-			o[cur-1] = o[cur] + 1;
+		if (cur == K)
+			break;
+		int v[3] = { cur + 1, cur - 1, cur * 2 };
+		for (i=0;i<3;i++) {
+			if (v[i] >= 0 && v[i] <= 100000 && visited[v[i]] == -1) {
+				q.push(v[i]);
+				visited[v[i]] = visited[cur] + 1;
+				p[v[i]] = cur;
+			}
 		}
 	}
 }
 int main() {
-	ios_base::sync_with_stdio(false);
-	int n = K, ans = 0, prev, pprev;
 	cin >> N >> K;
-	for (int i=0;i<=100000;i++) {
-		o[i] = INF;
-	}
+	fill(visited, visited+100001, -1);
+	
 	bfs();
-	n = K;
-	prev = K;
-	pprev = K;
-
-	while (n != N) {
-		pprev = prev;
-		prev = n;
-		n = m[n];
-		m[prev] = pprev;
-		ans++;
+	vector<int> ans;
+	int i = K;
+	 while (i != N) {
+		ans.push_back(i);
+		i = p[i];
 	}
-	m[n] = prev;
-	cout << ans << endl;
-	n = N;
-	cout << n << ' ';
-	while (n != K) {
-		cout << m[n] << ' ';
-		n = m[n];
+	ans.push_back(N);
+	reverse(ans.begin(), ans.end());
+	cout << ans.size() - 1 << endl;
+	for (int x: ans) {
+		cout << x << ' ';
 	}
 	cout << endl;
-
 	return 0;
 }
