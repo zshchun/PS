65264edff7433d49ba8ca2a5bedc9b56c2c303b6
Seunghwan Chun <zshchun@gmail.com>
2023-06-28 01:29:00 +0900

    BOJ 4344 평균은 넘겠지 (Bronze I)
    https://www.acmicpc.net/problem/4344

--- boj/Bronze I/4344.py
+++ boj/Bronze I/4344.py
@@ -7,20 +7,18 @@ n = int(input().encode('utf-8'))
 total = 0
 s = 0
 lines = []
-result = ""
 for i in range(n):
-	s = 0
-	total = 0
-	cnt = 0
-	buf = input().encode('utf-8')
-	lines.append(buf)
-	l = buf.split()
-	total += int(l[0])
-	for j in l[1:]:
-		s += int(j)
-	mid = s / total
-	for j in l[1:]:
-		if int(j) > mid:
-			cnt += 1
-	result += "{:.3f}%\n".format(cnt / int(l[0]) * 100)
-print(result)
+    s = 0
+    total = 0
+    cnt = 0
+    buf = input().encode('utf-8')
+    lines.append(buf)
+    l = buf.split()
+    total += int(l[0])
+    for j in l[1:]:
+        s += int(j)
+    mid = s / total
+    for j in l[1:]:
+        if int(j) > mid:
+            cnt += 1
+    print("{:.3f}%".format(cnt / int(l[0]) * 100))
