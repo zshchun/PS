20a73a0d1c9d01f261a3bb6225e3739d99660fcd
Seunghwan Chun <zshchun@gmail.com>
2023-10-04 22:16:20 +0900

    BOJ 25083 새싹 (Bronze V)
    https://www.acmicpc.net/problem/25083

---
+++ boj/Bronze V/25083.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+
+	return 0;
+}
