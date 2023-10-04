ce04f1a7d23d69215e471791de74d3474e7df58f
Seunghwan Chun <zshchun@gmail.com>
2023-10-04 22:16:24 +0900

    BOJ 27866 문자와 문자열 (Bronze V)
    https://www.acmicpc.net/problem/27866

---
+++ boj/Bronze V/27866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	int i;
+	cin >> s >> i;
+	cout << s[i-1] << endl;
+	return 0;
+}
