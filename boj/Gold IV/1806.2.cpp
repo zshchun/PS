d0d8b1932f66d3b7864f6551b0ce5bbb6cb49e72
Seunghwan Chun <zshchun@gmail.com>
2023-11-04 20:31:30 +0900

    BOJ 1806 부분합 (Gold IV)
    https://www.acmicpc.net/problem/1806

---
+++ boj/Gold IV/1806.2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 12345678912345ULL
+ll N, S;
+int a[100001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll i, j, l, r, sum, ans = INF;
+	cin >> N >> S;
+	for (i=0;i<N;i++)
+		cin >> a[i];
+	l = 0;
+	r = 0;
+	sum = 0;
+	while (l <= r) {
+		if (sum <= S && r != N) {
+			sum += a[r];
+			r++;
+		} else {
+			sum -= a[l];
+			l++;
+		}
+		if (sum >= S && ans > r - l) {
+			ans = r - l;
+		}
+	}
+	if (ans == INF)
+		cout << "0\n";
+	else
+		cout << ans << endl;
+	return 0;
+}
