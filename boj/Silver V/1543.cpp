e4fadc8508bf800b6d948ead66849da3a2fccbcb
Seunghwan Chun <zshchun@gmail.com>
2025-01-03 21:33:15 +0900

    BOJ 1543 문서 검색 (Silver V)
    https://www.acmicpc.net/problem/1543

--- boj/Silver V/1543.cpp
+++ boj/Silver V/1543.cpp
@@ -8,22 +8,5 @@ typedef pair<ll,ll> pa;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	int pos = 0, ans = 0;
-	string a;
-	string b;
-
-	getline(cin, a);
-	getline(cin, b);
-
-	while (pos < a.size()) {
-		if (a.substr(pos, b.size()) == b) {
-			ans++;
-			pos += b.size();
-		} else {
-			pos++;
-		}
-	}
-	cout << ans << endl;
-
 	return 0;
 }
