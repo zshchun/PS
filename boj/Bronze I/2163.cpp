70ecdfa018436fe7badcc6d23dfb9021cdf5c22d
Seunghwan Chun <zshchun@gmail.com>
2025-01-16 23:46:35 +0900

    BOJ 2163 초콜릿 자르기 (Bronze I)
    https://www.acmicpc.net/problem/2163

---
+++ boj/Bronze I/2163.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	cout << n * m - 1 << endl;
+	return 0;
+}
