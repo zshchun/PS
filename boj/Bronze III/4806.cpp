b5a03fec9c88d7345bf322a85d65bda5c4bb2b3e
Seunghwan Chun <zshchun@gmail.com>
2025-02-07 23:38:59 +0900

    BOJ 4806 줄 세기 (Bronze III)
    https://www.acmicpc.net/problem/4806

---
+++ boj/Bronze III/4806.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int ans = 0;
+	string s;
+	while (getline(cin, s)) {
+		
+		ans++;
+	}
+	cout << ans << endl;
+	return 0;
+}
