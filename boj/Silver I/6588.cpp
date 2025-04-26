9a95ea509462cc495d2e032817244d08771bc408
Seunghwan Chun <zshchun@gmail.com>
2025-04-26 20:16:43 +0900

    BOJ 6588 골드바흐의 추측 (Silver I)
    https://www.acmicpc.net/problem/6588

--- boj/Silver I/6588.cpp
+++ boj/Silver I/6588.cpp
@@ -8,27 +8,28 @@ typedef pair<ll,ll> pa;
 bool prime[1000001];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int i, j;
-	fill_n(prime, 1000001, 1);
-	prime[1] = 0;
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        int i, j;
+        fill_n(prime, 1000001, 1);
+        prime[1] = 0;
 
-	for (i=2;i<=1000000;i++) {
-		for (j=i+i;j<=1000000;j+=i) {
-			prime[j] = 0;
-		}
-	}
+        for (i=2;i<=1000000;i++) {
+                if (prime[i] == 0) continue;
+                for (j=i+i;j<=1000000;j+=i) {
+                        prime[j] = 0;
+                }
+        }
 
-	while (1) {
-		cin >> i;
-		if (i == 0) break;
-		for (j=1;j<i;j++) {
-			if (prime[j] && prime[i-j]) {
-				cout << i << " = " << j << " + " << i-j << endl;
-				break;
-			}
-		}
-	}
-	return 0;
+        while (1) {
+                cin >> i;
+                if (i == 0) break;
+                for (j=1;j<i;j++) {
+                        if (prime[j] && prime[i-j]) {
+                                cout << i << " = " << j << " + " << i-j << endl;
+                                break;
+                        }
+                }
+        }
+        return 0;
 }
