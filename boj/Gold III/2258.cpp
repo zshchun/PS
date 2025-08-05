c244b6ad6ed937ac8ff45d154940abd4adcc7a0f
Seunghwan Chun <zshchun@gmail.com>
2025-08-06 00:21:58 +0900

    BOJ 2258 정육점 (Gold III)
    https://www.acmicpc.net/problem/2258

--- boj/Gold III/2258.cpp
+++ boj/Gold III/2258.cpp
@@ -9,35 +9,34 @@ ll n, m, ans = INF;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	ll i, price, sum, cur, cur_price;
+	ll i, price, sum, cur;
 	cin >> n >> m;
 	vector<pa> a(n);
+
 	for (i=0;i<n;i++)
 		cin >> a[i].second >> a[i].first;
+
 	sort(a.begin(), a.end(), [&](const pa lhs, const pa rhs) {
 		if (lhs.first == rhs.first)
 			return lhs.second > rhs.second;
 		return lhs.first < rhs.first;
 	});
 
-	price = -1;
-	cur = 0;
-	sum = 0;
+
+	price = sum = cur = 0;
 	for (i=0;i<n;i++) {
-		if (a[i].first != price) {
+		sum += a[i].second;
+		if (i && a[i].first == a[i-1].first)
+			price += a[i].first;
+		else
 			price = a[i].first;
-			cur_price = a[i].first;
-			sum += cur;
-			cur = a[i].second;
-		} else {
-			cur_price += a[i].first;
-			cur += a[i].second;
-		}
-		if (sum+cur >= m)
-			ans = min(ans, cur_price);
+		if (sum >= m)
+			ans = min(ans, price);
 	}
+
 	if (ans == INF)
 		ans = -1;
+
 	cout << ans << endl;
 	return 0;
 }
