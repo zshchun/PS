ca7cd2b5da751cc3b88c32a46643c530578960ca
Seunghwan Chun <zshchun@gmail.com>
2025-08-24 21:48:47 +0900

    BOJ 13297 Quick Estimates (Bronze IV)
    https://www.acmicpc.net/problem/13297

---
+++ boj/Bronze IV/13297.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n, i;
+	cin >> n;
+	string s;
+	for (i=0;i<n;i++) {
+		cin >> s;
+		cout << s.size() << endl;
+	}
+	return 0;
+}
