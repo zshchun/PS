47b9e3161b5221d663eb365a2b527408e4f87bce
Seunghwan Chun <zshchun@gmail.com>
2025-11-15 21:31:06 +0900

    BOJ 23813 회전 (Bronze II)
    https://www.acmicpc.net/problem/23813

--- boj/Bronze II/23813.cpp
+++ boj/Bronze II/23813.cpp
@@ -8,14 +8,5 @@ typedef pair<ll,ll> pa;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	ll i, n, ans = 0;
-	string s;
-	cin >> s;
-	for (i=0;i<s.size();i++) {
-		ans += stol(s);
-		s = s.back() + s.substr(0, s.size()-1);;
-	}
-	cout << ans << endl;
-
 	return 0;
 }
