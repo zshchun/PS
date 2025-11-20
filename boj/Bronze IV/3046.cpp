9bee95e82bdea2822611c83f59286cbd8543719a
Seunghwan Chun <zshchun@gmail.com>
2025-11-21 00:47:41 +0900

    BOJ 3046 R2 (Bronze IV)
    https://www.acmicpc.net/problem/3046

---
+++ boj/Bronze IV/3046.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int r1, r2, s;
+	cin >> r1 >> s;
+	r2 = s * 2 - r1;
+	cout << r2 << endl;
+	return 0;
+}
