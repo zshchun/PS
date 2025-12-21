7683c31269526ebd01b5da27309d1efb6a515038
Seunghwan Chun <zshchun@gmail.com>
2025-12-22 01:57:06 +0900

    BOJ 2026 소풍 (Gold II)
    https://www.acmicpc.net/problem/2026

---
+++ boj/Gold II/2026.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+bool g[901][901];
+bool used[901];
+int cnt[901];
+int K, N, F;
+
+bool is_mutual_friend(int x) {
+	for (int i=1;i<x;i++)
+		if (used[i] && !g[x][i])
+			return false;
+	return true;
+}
+
+void backtrack(int x, int k) {
+	if (!is_mutual_friend(x))
+		return;
+
+	used[x] = 1;
+	if (k == K) {
+		for (int i=1;i<=N;i++)
+			if (used[i])
+				cout << i << endl;
+		exit(0);
+	}
+	for (int i=x+1;i<=N;i++)
+		backtrack(i, k+1);
+	used[x] = 0;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int i, j, u, v;
+	cin >> K >> N >> F;
+	for (i=0;i<F;i++) {
+		cin >> u >> v;
+		g[v][u] = g[u][v] = 1;
+		cnt[u]++; cnt[v]++;
+	}
+
+	for (i=1;i<=N;i++) {
+		if (cnt[i] < K-1) continue;
+		backtrack(i, 1);
+	}
+
+	cout << "-1\n";
+	return 0;
+}
