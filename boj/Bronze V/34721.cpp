5dde78951962a7abc044d6600997e32c9dc28c09
Seunghwan Chun <zshchun@gmail.com>
2025-12-23 04:37:19 +0900

    BOJ 34721 역사를 걸으면 동국이 보이고 (Bronze V)
    https://www.acmicpc.net/problem/34721

---
+++ boj/Bronze V/34721.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int i, n;
+	cin >> n;
+	for (i=0;i<n;i++)
+		cout << "I love DGU\n";
+	return 0;
+}
