acac030577a13926d0bdf36017c5f37098d549c7
Seunghwan Chun <zshchun@gmail.com>
2026-01-04 21:35:04 +0900

    BOJ 28707 배열 정렬 (Gold I)
    https://www.acmicpc.net/problem/28707

--- boj/Gold I/28707.cpp
+++ boj/Gold I/28707.cpp
@@ -12,53 +12,55 @@ vector<int> a;
 vector<tup> op;
 
 bool is_sorted(vector<int> &v) {
-	for (int i=1;i<v.size();i++)
-		if (v[i-1] > v[i])
-			return false;
-	return true;
+    for (int i=1;i<v.size();i++)
+        if (v[i-1] > v[i])
+            return false;
+    return true;
 }
 
 void dijkstra() {
-	priority_queue<pv, vector<pv>, greater<pv>> pq;
-	map<vector<int>, int> visited;
-	pq.push(pv(0, a));
-	while (!pq.empty()) {
-		auto [cost, arr] = pq.top();
-		pq.pop();
-		if (is_sorted(arr))
-			ans = min(ans, cost);
+    priority_queue<pv, vector<pv>, greater<pv>> pq;
+    map<vector<int>, int> visited;
+    pq.push(pv(0, a));
+    while (!pq.empty()) {
+        auto [cost, arr] = pq.top();
+        pq.pop();
+        if (is_sorted(arr)) {
+            ans = min(ans, cost);
+            break;
+        }
 
-		for (auto &[l, r, c] : op) {
-			swap(arr[l], arr[r]);
-			if (visited.count(arr) == 0 || visited[arr] > cost + c) {
-				visited[arr] = cost + c;
-				pq.push(pv(cost + c, arr));
-			}
-			swap(arr[l], arr[r]);
-		}
-	}
+        for (auto &[l, r, c] : op) {
+            swap(arr[l], arr[r]);
+            if (visited.count(arr) == 0 || visited[arr] > cost + c) {
+                visited[arr] = cost + c;
+                pq.push(pv(cost + c, arr));
+            }
+            swap(arr[l], arr[r]);
+        }
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int i, l, r, c;
-	cin >> N;
-	a.resize(N);
-	for (i=0;i<N;i++)
-		cin >> a[i];
-	cin >> M;
-	op.resize(M);
-	for (i=0;i<M;i++) {
-		cin >> l >> r >> c;
-		l--; r--;
-		op[i] = tup(l, r, c);
-	}
-	dijkstra();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int i, l, r, c;
+    cin >> N;
+    a.resize(N);
+    for (i=0;i<N;i++)
+        cin >> a[i];
+    cin >> M;
+    op.resize(M);
+    for (i=0;i<M;i++) {
+        cin >> l >> r >> c;
+        l--; r--;
+        op[i] = tup(l, r, c);
+    }
+    dijkstra();
 
-	if (ans == INF)
-		cout << "-1\n";
-	else
-		cout << ans << endl;
-	return 0;
+    if (ans == INF)
+        cout << "-1\n";
+    else
+        cout << ans << endl;
+    return 0;
 }
