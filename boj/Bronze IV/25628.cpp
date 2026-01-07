a44037def02f134fca2028af3e21e412f97ad6ef
Seunghwan Chun <zshchun@gmail.com>
2026-01-07 23:26:16 +0900

    BOJ 25628 햄버거 만들기 (Bronze IV)
    https://www.acmicpc.net/problem/25628

---
+++ boj/Bronze IV/25628.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int a, b, ans;
+	cin >> a >> b;
+	ans = min(a/2, b);
+	cout << ans << endl;
+	return 0;
+}
