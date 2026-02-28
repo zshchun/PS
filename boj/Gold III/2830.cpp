31c048ce5b3298ae54c932b942d14e05367003cf
Seunghwan Chun <zshchun@gmail.com>
2026-02-28 14:36:37 +0900

    BOJ 2830 행성 X3 (Gold III)
    https://www.acmicpc.net/problem/2830

---
+++ boj/Gold III/2830.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+ll N, ans;
+ll zero[21];
+ll one[21];
+ll a[1000001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int i, j;
+	cin >> N;
+	for (i=0;i<N;i++) {
+		cin >> a[i];
+		for (j=0;j<20;j++)
+			if (a[i] & (1ull << j))
+				one[j]++;
+	}
+
+	for (j=0;j<20;j++)
+		zero[j] = N - one[j];
+
+	for (i=0;i<N;i++)
+		for (j=0;j<20;j++)
+			if (a[i] & (1<<j))
+				ans += zero[j] * (1ull << j);
+
+	cout << ans << endl;
+	return 0;
+}
