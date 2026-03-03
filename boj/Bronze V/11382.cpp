7d8ecd23cd3c975d7ecfa00a5c2842c86e443280
Seunghwan Chun <zshchun@gmail.com>
2026-03-03 23:47:16 +0900

    BOJ 11382 꼬마 정민 (Bronze V)
    https://www.acmicpc.net/problem/11382

---
+++ boj/Bronze V/11382.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ll a, b, c, ans;
+	cin >> a >> b >> c;
+	ans = a + b + c;
+	cout << ans << endl;
+
+	return 0;
+}
