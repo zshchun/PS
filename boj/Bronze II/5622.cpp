c1c430f9bd0df0c2058d99a4417c2ebea2a311f4
Seunghwan Chun <zshchun@gmail.com>
2026-03-06 01:29:40 +0900

    BOJ 5622 다이얼 (Bronze II)
    https://www.acmicpc.net/problem/5622

--- boj/Bronze II/5622.cpp
+++ boj/Bronze II/5622.cpp
@@ -1,21 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pa;
+#define endl "\n"
+#define INF 123456789
+
 int main() {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int ans = 0, pos = 0;
 	string s;
-	int i, j, sum=0, gap;
 	cin >> s;
-	char c;
-	for (i=0;i<s.size();i++) {
-		c = s[i]-'A';
-		if (c > 22) {
-			c -= 2;
-		} else if (c > 15) {
-			c -= 1;
+	for (char &c : s) {
+		switch (c) {
+			case 'A' ... 'C':
+				ans += 3;
+			break;
+			case 'D' ... 'F':
+				ans += 4;
+			break;
+			case 'G' ... 'I':
+				ans += 5;
+			break;
+			case 'J' ... 'L':
+				ans += 6;
+			break;
+			case 'M' ... 'O':
+				ans += 7;
+			break;
+			case 'P' ... 'S':
+				ans += 8;
+			break;
+			case 'T' ... 'V':
+				ans += 9;
+			break;
+			case 'W' ... 'Z':
+				ans += 10;
+			break;
 		}
-		j = c / 3;
-		sum += j + 3;
 	}
-	cout << sum << "\n";
+	cout << ans << endl;
 	return 0;
 }
