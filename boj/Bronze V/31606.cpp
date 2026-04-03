58999ecf39085ee984397b15b077253a72dd06a9
Seunghwan Chun <zshchun@gmail.com>
2026-04-03 19:56:03 +0900

    BOJ 31606 果物 (Fruit) (Bronze V)
    https://www.acmicpc.net/problem/31606

---
+++ boj/Bronze V/31606.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int x, y;
+	cin >> x >> y;
+	cout << x + y + 3 << endl;
+	return 0;
+}
