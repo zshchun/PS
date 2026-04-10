ce9accef5b7b27c3cbc9c619d7bf98dc472e2dbd
Seunghwan Chun <zshchun@gmail.com>
2026-04-10 17:32:13 +0900

    BOJ 16600 Contemporary Art (Bronze IV)
    https://www.acmicpc.net/problem/16600

---
+++ boj/Bronze IV/16600.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	double a, x;
+	cin >> a;
+	x = sqrt(a) * 4;
+	cout << fixed << setprecision(6) << x << endl;
+	return 0;
+}
