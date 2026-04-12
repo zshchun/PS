85935d53df316e21fcc6b90df4979cf4c5190fa8
Seunghwan Chun <zshchun@gmail.com>
2026-04-13 00:24:48 +0900

    BOJ 32246 빙고 막기 (Bronze III)
    https://www.acmicpc.net/problem/32246

---
+++ boj/Bronze III/32246.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	if (n == 2)
+		cout << "3\n";
+	else
+		cout << n << endl;
+	return 0;
+}
