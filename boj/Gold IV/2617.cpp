950d8be816474a93a824cfbe0f82afb77dd28974
Seunghwan Chun <zshchun@gmail.com>
2026-04-21 01:25:11 +0900

    BOJ 2617 구슬 찾기 (Gold IV)
    https://www.acmicpc.net/problem/2617

--- boj/Gold IV/2617.cpp
+++ boj/Gold IV/2617.cpp
@@ -6,34 +6,24 @@ typedef pair<int,int> pi;
 #define endl "\n"
 #define INF 123456789
 int N, M, ans;
-int d[100][100];
-int dr[100][100];
+bool d[100][100];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	int i, j, k, u, v;
 	cin >> N >> M;
-	fill_n(d[0], 100*100, INF);
-	fill_n(dr[0], 100*100, INF);
 
 	for (i=0;i<M;i++) {
 		cin >> u >> v;
 		d[u][v] = 1;
-		dr[v][u] = 1;
-	}
-	for (i=1;i<=N;i++) {
-		d[i][i] = 0;
-		dr[i][i] = 0;
 	}
 
 	for (k=1;k<=N;k++) {
 		for (i=1;i<=N;i++) {
 			for (j=1;j<=N;j++) {
-				if (d[i][j] > d[i][k] + d[k][j])
-					d[i][j] = d[i][k] + d[k][j];
-				if (dr[i][j] > dr[i][k] + dr[k][j])
-					dr[i][j] = dr[i][k] + dr[k][j];
+				if (d[i][k] && d[k][j])
+					d[i][j] = 1;
 			}
 		}
 	}
@@ -43,8 +33,8 @@ int main() {
 		int cnt2 = 0;
 		for (j=1;j<=N;j++) {
 			if (i == j) continue;
-			if (d[i][j] != INF) cnt1++;
-			if (dr[i][j] != INF) cnt2++;
+			if (d[i][j]) cnt1++;
+			if (d[j][i]) cnt2++;
 		}
 		if (cnt1 > (N-1) / 2 || cnt2 > (N-1) / 2)
 			ans++;
