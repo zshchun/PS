0575fcf9b7fd0648260feab6168512ee013b89df
Seunghwan Chun <zshchun@gmail.com>
2026-04-22 23:57:49 +0900

    BOJ 4101 크냐? (Bronze V)
    https://www.acmicpc.net/problem/4101

---
+++ boj/Bronze V/4101.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (!a && !b) break;
+		if (a > b)
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
+	return 0;
+}
