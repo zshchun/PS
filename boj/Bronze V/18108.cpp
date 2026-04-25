5958a60a7d5ab40016febfa86f9736bc4b2815cf
Seunghwan Chun <zshchun@gmail.com>
2026-04-25 22:37:26 +0900

    BOJ 18108 1998년생인 내가 태국에서는 2541년생?! (Bronze V)
    https://www.acmicpc.net/problem/18108

---
+++ boj/Bronze V/18108.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+#define endl "\n"
+#define INF 123456789
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int y;
+	cin >> y;
+	cout << y - 543 << endl;
+	return 0;
+}
